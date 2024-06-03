e4fb979662a8ccaf15860ff8ce40d33bc931e8f3
kihoonnamdev <kihoon.nam.dev@gmail.com>
2024-06-04 06:23:43 +0900

    Chapter 1~3 Run App & Social Auth & Header

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
 }
