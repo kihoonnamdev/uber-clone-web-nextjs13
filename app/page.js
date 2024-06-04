1128b9f729c7e974e421b4ce8448dda8ec5f687c
kihoonnamdev <kihoon.nam.dev@gmail.com>
2024-06-05 06:56:25 +0900

    Chapter 8 Get Source/Destination Latitude & Longitude

--- app/page.js
+++ app/page.js
@@ -1,17 +1,29 @@
+"use client";
+
 //import { UserButton } from "@clerk/nextjs";
 import GoogleMapSection from "@/components/Home/GoogleMapSection";
 import SearchSection from "@/components/Home/SearchSection";
+import { DestinationContext } from "@/context/DestinationContext";
+import { SourceContext } from "@/context/SourceContext";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [source, setSource] = useState([]);
+  const [destination, setDestination] = useState([]);
+
   return (
-    <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
-      <div>
-        <SearchSection />
-      </div>
-      <div className="col-span-2">
-        <GoogleMapSection />
-      </div>
-    </div>
+    <SourceContext.Provider value={{ source, setSource }}>
+      <DestinationContext.Provider value={{ destination, setDestination }}>
+        <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+          <div>
+            <SearchSection />
+          </div>
+          <div className="col-span-2">
+            <GoogleMapSection />
+          </div>
+        </div>
+      </DestinationContext.Provider>
+    </SourceContext.Provider>
   );
 }
