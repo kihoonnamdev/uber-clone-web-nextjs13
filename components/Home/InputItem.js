274bdffa20920188d9d3d51061835ae5b83f0b93
kihoonnamdev <kihoon.nam.dev@gmail.com>
2024-06-05 05:13:56 +0900

    Chapter 7 Search Place Autocomplete

--- components/Home/InputItem.js
+++ components/Home/InputItem.js
@@ -1,7 +1,10 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 function InputItem({ type }) {
+  const [value, setValue] = useState(null);
   return (
     <div className="bg-slate-200 p-3 rounded mt-3 flex items-center gap-4">
       <Image
@@ -9,10 +12,30 @@ function InputItem({ type }) {
         width={15}
         height={15}
       ></Image>
-      <input
+      {/* <input
         type="text"
         placeholder={type == "source" ? "Pickup Location" : "DropOff Location"}
         className="bg-transparent w-full outline-none"
+      /> */}
+      <GooglePlacesAutocomplete
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+        selectProps={{
+          value,
+          onChange: setValue,
+          placeholder: "Pickup Location",
+          isClearable: true,
+          className: "w-full",
+          components: {
+            DropdownIndicator: false,
+          },
+          styles: {
+            control: (provided) => ({
+              ...provided,
+              backgroundColor: "#00ffff00",
+              border: "none",
+            }),
+          },
+        }}
       />
     </div>
   );
