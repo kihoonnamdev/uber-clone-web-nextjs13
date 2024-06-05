ac25166a08584af986d3de46bf72d1feb0fb9da4
kihoonnamdev <kihoon.nam.dev@gmail.com>
2024-06-05 09:25:18 +0900

    Chapter 10 Integrate Google Map

--- components/Home/GoogleMapSection.js
+++ components/Home/GoogleMapSection.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 function GoogleMapSection() {
-  return <div>GoogleMapSection</div>;
+  const [containerStyle, setContainerStyle] = useState({
+    width: "100%",
+    height: "60vh",
+  });
+  //   const containerStyle = {
+  //     width: "100%",
+  //     height: window.innerWidth * 0.6,
+  //   };
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setContainerStyle({ width: "100%", height: window.innerWidth * 0.6 });
+    }
+  }, []);
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523,
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      options={{ mapId: "9f4b478a260cc20f" }}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
 
 export default GoogleMapSection;
